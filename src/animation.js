c2c33dbf13dd43c97e14f43d9c445e5c3105dbf0
Jianing <jianingw321@outlook.com>
2020-11-10 16:18:43 +0800

    15. wave line animation

--- src/animation.js
+++ src/animation.js
@@ -35,6 +35,7 @@ export const fade = {
   show: { opacity: 1, transition: { duration: 0.5, ease: "easeOut" } },
 };
 
+//? i don't like it
 export const photoAnim = {
   hidden: { scale: 1.5, opacity: 0 },
   show: { scale: 1, opacity: 1, transition: { duration: 1, ease: "easeOut" } },
