a8faf8e05561755350b66bf34a1dc6a398b24347
Jianing <jianingw321@outlook.com>
2020-11-10 15:40:09 +0800

    12. set up Framer Motion and title motion

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -2,24 +2,50 @@ import React from "react";
 import home1 from "../img/home1.png";
 //Style
 import { About, Description, Image, Hide } from "../styles";
+//Framer Motion
+import { motion } from "framer-motion";
 
 const AboutSection = () => {
+  //Motion Variants
+  const titleAnim = {
+    hidden: { opacity: 0 },
+    show: { opacity: 1, transition: { duration: 2 } },
+  };
+
+  const titleContainer = {
+    hidden: { x: 100 },
+    show: {
+      x: 0,
+      transition: {
+        duration: 1,
+        ease: "easeOut",
+        staggerChildren: 0.75,
+        when: "beforeChildren",
+      },
+    },
+  };
+
   return (
     <About>
       <Description>
-        <div className="title">
+        <motion.div
+          variants={titleContainer}
+          initial="hidden"
+          animate="show"
+          className="title"
+        >
           <Hide>
-            <h2>We work to make</h2>
+            <motion.h2 variants={titleAnim}>We work to make</motion.h2>
           </Hide>
           <Hide>
-            <h2>
+            <motion.h2 variants={titleAnim}>
               your <span>dreams</span> come
-            </h2>
+            </motion.h2>
           </Hide>
           <Hide>
-            <h2>true.</h2>
+            <motion.h2 variants={titleAnim}>true.</motion.h2>
           </Hide>
-        </div>
+        </motion.div>
         <p>
           Contact us for any photography or videography ideas that you have. We
           have professionals with amazing skills.
